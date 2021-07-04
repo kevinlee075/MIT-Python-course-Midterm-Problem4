3637dac164e0ecf7b2c1794847abe1edffc1120c
kevinlee075 <kevinlee075@gmail.com>
2021-07-04 16:38:59 +0800

    Update MIT-Python-course-Midterm-problem4.py

--- MIT-Python-course-Midterm-problem4.py
+++ MIT-Python-course-Midterm-problem4.py
@@ -5,6 +5,9 @@ Created on Sun Jul  4 16:21:34 2021
 @author: Lenovo
 """
 
+#Implement a function that meets the specifications below.
+#For example, if L = [[1, 2], [3, 4], [5, 6, 7]] then deep_reverse(L) mutates L to be [[7, 6, 5], [4, 3], [2, 1]]
+
 def deep_reverse(L):
     """ assumes L is a list of lists whose elements are ints
     Mutates L such that it reverses its elements and also 
